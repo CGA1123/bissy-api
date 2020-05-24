03d43b03431b0ea1c6e89e57993b499fd844a7e8
Christian Gregg <c_arlt@hotmail.com>
2020-05-24 23:32:36 +0100

    use PORT env

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/cga1123/bissy-api/handlers"
@@ -11,9 +12,13 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/ping", handlers.Ping)
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
 
 	server := &http.Server{
-		Addr:         "0.0.0.0:8080",
+		Addr:         ":" + port,
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
