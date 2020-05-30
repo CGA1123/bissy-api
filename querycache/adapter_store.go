3fbc66dea7a162794089331bb7e9b4cbd4e99737
Christian Gregg <c_arlt@hotmail.com>
2020-05-30 12:06:42 +0100

    AdapterStore interface

--- querycache/adapter_store.go
+++ querycache/adapter_store.go
@@ -22,3 +22,11 @@ type CreateAdapter struct {
 	Type    string `json:"type"`
 	Options string `json:"options"`
 }
+
+type AdapterStore interface {
+	Get(string) (*Adapter, error)
+	Create(*CreateAdapter) (*Adapter, error)
+	List(page int, per int) ([]Adapter, error)
+	Delete(string) (*Adapter, error)
+	Update(string, *UpdateAdapter) (*Adapter, error)
+}
