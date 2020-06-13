307b5e216e1d88087f2184f902dba66bd80c32e7
Christian Gregg <c_arlt@hotmail.com>
2020-06-13 15:54:55 +0100

    misc: golint

--- tools.go
+++ tools.go
@@ -3,6 +3,8 @@
 package tools
 
 import (
+	// importing to allow building on migrate cli on heroku
 	_ "github.com/golang-migrate/migrate/v4"
+	// importing to allow building on migrate cli on heroku
 	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"
 )
