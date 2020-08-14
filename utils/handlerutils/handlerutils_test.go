70ee90b8ac564db3baf49860cc7c90c9cc9a1797
Christian Gregg <c_arlt@hotmail.com>
2020-08-14 17:42:33 +0100

    handlerutils: improve coverage

--- utils/handlerutils/handlerutils_test.go
+++ utils/handlerutils/handlerutils_test.go
@@ -1,12 +1,16 @@
 package handlerutils_test
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/cga1123/bissy-api/utils/expect"
+	"github.com/cga1123/bissy-api/utils/expecthttp"
 	"github.com/cga1123/bissy-api/utils/handlerutils"
+	"github.com/gorilla/mux"
 )
 
 func TestRequire(t *testing.T) {
@@ -75,4 +79,100 @@ func TestGet(t *testing.T) {
 	expect.Equal(t, 1123, isIntValue)
 	expect.Equal(t, 3211, notIntValue)
 	expect.Equal(t, 3211, notExistValue)
+}
+
+func TestParams(t *testing.T) {
+	t.Parallel()
+
+	recorder := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		params := handlerutils.Params(r)
+
+		pathParam, pathOk := params.Get("pathParam")
+		queryParam, queryOk := params.Get("queryParam")
+
+		expect.True(t, pathOk)
+		expect.Equal(t, "path-parameter", pathParam)
+
+		expect.True(t, queryOk)
+		expect.Equal(t, "query-parameter", queryParam)
+
+	}
+
+	router.HandleFunc("/test/{pathParam}", handler).Methods("GET")
+
+	request, err := http.NewRequest("GET", "/test/path-parameter?queryParam=query-parameter", nil)
+	expect.Ok(t, err)
+
+	router.ServeHTTP(recorder, request)
+}
+
+func TestHandler(t *testing.T) {
+	t.Parallel()
+
+	// no error
+	handler := &handlerutils.Handler{
+		H: func(w http.ResponseWriter, r *http.Request) error { return nil }}
+	sqlErrHandler := &handlerutils.Handler{
+		H: func(w http.ResponseWriter, r *http.Request) error { return sql.ErrNoRows }}
+	handlerErrHandler := &handlerutils.Handler{H: func(w http.ResponseWriter, r *http.Request) error {
+		return &handlerutils.HandlerError{Err: fmt.Errorf("error"), Status: http.StatusBadRequest}
+	}}
+
+	otherErrHandler := &handlerutils.Handler{H: func(w http.ResponseWriter, r *http.Request) error {
+		return fmt.Errorf("error")
+	}}
+
+	router := mux.NewRouter()
+	router.Handle("/handler", handler).Methods("GET")
+	router.Handle("/sqlErrHandler", sqlErrHandler).Methods("GET")
+	router.Handle("/handlerErrHandler", handlerErrHandler).Methods("GET")
+	router.Handle("/otherErrHandler", otherErrHandler).Methods("GET")
+
+	handlerRequest, err := http.NewRequest("GET", "/handler", nil)
+	expect.Ok(t, err)
+
+	sqlErrHandlerRequest, err := http.NewRequest("GET", "/sqlErrHandler", nil)
+	expect.Ok(t, err)
+
+	handlerErrHandlerRequest, err := http.NewRequest("GET", "/handlerErrHandler", nil)
+	expect.Ok(t, err)
+
+	otherErrHandlerRequest, err := http.NewRequest("GET", "/otherErrHandler", nil)
+	expect.Ok(t, err)
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, handlerRequest)
+	expecthttp.Ok(t, recorder)
+
+	recorder = httptest.NewRecorder()
+	router.ServeHTTP(recorder, sqlErrHandlerRequest)
+	expecthttp.Status(t, http.StatusNotFound, recorder)
+
+	recorder = httptest.NewRecorder()
+	router.ServeHTTP(recorder, handlerErrHandlerRequest)
+	expecthttp.Status(t, http.StatusBadRequest, recorder)
+
+	recorder = httptest.NewRecorder()
+	router.ServeHTTP(recorder, otherErrHandlerRequest)
+	expecthttp.Status(t, http.StatusInternalServerError, recorder)
+}
+
+func TestContentType(t *testing.T) {
+	t.Parallel()
+
+	handler := func(w http.ResponseWriter, r *http.Request) { handlerutils.ContentType(w, "testing") }
+
+	router := mux.NewRouter()
+	router.HandleFunc("/test/{pathParam}", handler).Methods("GET")
+
+	request, err := http.NewRequest("GET", "/test/path-parameter?queryParam=query-parameter", nil)
+	expect.Ok(t, err)
+
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+
+	expecthttp.Header(t, "Content-Type", "testing", recorder.Header())
 }
